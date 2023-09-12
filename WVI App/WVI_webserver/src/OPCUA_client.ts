c42de42d317735b55ed1884a078ccb9c53a9a8a4
Rémon Zander (0948619) <0948619@hr.nl>
2023-09-12 14:26:17 +0200

    OPCUA middelware toegevoegd en connectie met server gemaakt

---
+++ WVI App/WVI_webserver/src/OPCUA_client.ts
@@ -0,0 +1,92 @@
+import { Request, Response } from "express";
+import { Session } from "inspector";
+import { AttributeIds, BrowseResult, ClientSession, DataValue, OPCUAClient, ReferenceDescription, StatusCodes, TimestampsToReturn, UserTokenType } from "node-opcua-client";
+
+export default class OPCUAclient {
+
+    async Connect(req: Request, res: Response, next) {
+        let client: OPCUAClient;
+        let session: ClientSession;
+        const endpoint = "opc.tcp://game-pc:53530/OPCUA/SimulationServer";
+        const nodeId = "ns=3;s=test3";
+        res.setHeader('Content-Type', 'text/html');
+        res.write("starting OPCUA connection...\n");
+
+        try {
+            client = OPCUAClient.create({
+                endpointMustExist: false,
+                connectionStrategy: {
+                    maxRetry: 2,
+                    initialDelay: 2000,
+                    maxDelay: 10 * 1000
+                },
+            });
+            client.on("backoff", () => console.log("retrying connection"));
+
+
+            await client.connect(endpoint);
+
+            session = await client.createSession({
+                type: UserTokenType.UserName,
+                userName: "admin",
+                password: "admin",
+            });
+
+            res.write("connected!\n");
+            
+            const browseResult: BrowseResult = await session.browse("RootFolder") as BrowseResult;
+
+            console.log(browseResult.references.map((r: ReferenceDescription) => r.browseName.toString()).join("\n"));
+
+            const dataValue = await session.read({ nodeId, attributeId: AttributeIds.Value });
+
+            if (dataValue.statusCode !== StatusCodes.Good) {
+                console.log("Could not read ", nodeId);
+            }
+            console.log(` value = ${dataValue.value.toString()}`);
+
+            const subscription = await session.createSubscription2({
+                requestedPublishingInterval: 1000,
+                requestedLifetimeCount: 100,
+                requestedMaxKeepAliveCount: 20,
+                maxNotificationsPerPublish: 10,
+                publishingEnabled: true,
+                priority: 10
+            });
+
+            subscription
+                .on("started", () => console.log("subscription started - subscriptionId=", subscription.subscriptionId))
+                .on("keepalive", () => console.log("keepalive"))
+                .on("terminated", () => console.log("subscription terminated"));
+
+            const monitoredItem = await subscription.monitor({
+                nodeId,
+                attributeId: AttributeIds.Value
+            },
+                {
+                    samplingInterval: 100,
+                    discardOldest: true,
+                    queueSize: 10
+                }, TimestampsToReturn.Both);
+
+
+            monitoredItem.on("changed", (dataValue: DataValue) => {
+                console.log(` value = ${dataValue.value.value.toString()}`),
+                    res.write(` value = ${dataValue.value.value.toString()}\n`)
+            });
+
+            await new Promise((resolve) => setTimeout(resolve, 10000));
+            await subscription.terminate();
+
+            res.write("done!\n");
+            res.end();
+        }
+        catch (err) {
+            res.write(`Error !!! ${err}\n`);
+            res.end();
+            console.log("Error !!!", err);
+            session.close();
+            client.disconnect();
+        }
+    }
+}
---
+++ WVI App/WVI_webserver/src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+import { AddressInfo } from "net";
+import OPCUAclient from './OPCUA_client';
+
+const _OPCUAclient = new OPCUAclient();
+
+const app: Express = express();
+
+app.use('/OPCUA/connect', _OPCUAclient.Connect);
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Route is working!");
+})
+
+const server = app.listen(app.get('port'), function () {
+    console.log(`Express server listening on port ${(server.address() as AddressInfo).port}`);
+});
